d681a1119b16081e7e04dd25600b6854dcce3a9c
Casper Chia <casperchiakyechuen@gmail.com>
2016-06-04 00:24:38 +1000

    Remove unused code

---
+++ src/app/leaderboards/leaderboards.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Casper on 03/06/2016.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'leaderboards',
+    template: require('./leaderboards.component.html'),
+})
+
+export class LeaderboardsComponent{
+    constructor(){}
+}
