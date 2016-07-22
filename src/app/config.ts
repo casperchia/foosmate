45af29ab06dacea6aee9ab5ecf640976c9a19fda
cchia <casperchiakyechuen@gmail.com>
2016-07-22 13:24:06 +1000

    Split leaderboards into active and inactive players

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,6 @@
 var config= {
-    GAME_DURATION: 300,
+    GAME_DURATION: 1,
+    PLACEMENT_GAMES_REQUIRED: 5,
     API_URL: 'http://10.8.8.225:4000'
 }
 
--- src/app/leaderboards/leaderboards.component.ts
+++ src/app/leaderboards/leaderboards.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
 import {PlayerService} from "../../services/player.service";
 import {Player} from "../../models/player.model";
 import {OrderByRankPipe} from "../../pipes/order-by-rank.pipe";
+import config from "../config";
 
 @Component({
    selector: 'leaderboards',
@@ -14,14 +15,31 @@ import {OrderByRankPipe} from "../../pipes/order-by-rank.pipe";
 })
 
 export class LeaderboardsComponent{
+   PLACEMENT_GAMES_REQUIRED: number = config.PLACEMENT_GAMES_REQUIRED;
    players: Player[];
+   activePlayers: Player[];
+   inactivePlayers: Player[];
 
    constructor(private playerService: PlayerService){}
 
    ngOnInit(){
       this.playerService.getPlayers().subscribe(
-         players => this.players = players
+         players => this.initPlayerLists(players)
       )
+   }
+
+   initPlayerLists(players: Player[]){
+      this.activePlayers = [];
+      this.inactivePlayers = [];
+
+      for(let player of players){
+         if(player.rankedMatches.length <= this.PLACEMENT_GAMES_REQUIRED){
+            this.inactivePlayers.push(player);
+         }else{
+            this.activePlayers.push(player);
+         }
+      }
+
    }
 
 }
